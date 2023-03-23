7a4d29440b4c5e9502be4388c89afb100fe7d6b4
ghadahassann <alhamdullahalhamdullah93@gmail.com>
2023-03-23 12:06:34 -0700

    abs fun from stdlib.h

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _abs - computes the absolute value of an integer
+ * @c: integer
+ * Return: the abs value
+ */
+int _abs(int c)
+{
+	int x;
+
+	x = abs(c);
+	return (x);
+}
