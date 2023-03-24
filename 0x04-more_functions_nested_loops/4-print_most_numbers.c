d62aa8fb7fb824474ec1555de0c16bc068f42a2e
ghadahassann <alhamdullahalhamdullah93@gmail.com>
2023-03-23 19:59:32 -0700

    .

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_most_numbers - print numbers 0 to 9 not 2 and 4
+ *
+ *
+ */
+void print_most_numbers(void)
+{
+	int c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		if ( c == '2' || c == '4')
+		{
+			continue;
+		}
+		else
+		{
+			_putchar(c);
+			_putchar('\n');
+		}
+	}
+}
